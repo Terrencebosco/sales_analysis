2b75c8dbc210a2fe2051193c86e3e6c9467aac44
Terrence Bosco <terrencebosco@gmail.com>
2021-01-01 17:08:50 -0500

    added new graph, customer type and name by total sales amount for year

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ server = app.server
 months = ['January','February','March','April','May','June'
         ,'July','August','September','October','November','December']
 
+## do this before loading in data ##
 df = pd.read_csv('db_csv.csv')
 df = df[df['year']!=2017]
 
@@ -33,13 +34,25 @@ app.layout = dbc.Container([
 
     dbc.Row([
 
-        dbc.Col([
+            dbc.Col([
 
+            dcc.Checklist(id='year2', value=[2018,2019,2020],
+                          options=[{'label':x, 'value':x}
+                                   for x in sorted(month_year_group['year'].unique())],
+                          labelClassName="mr-3"),
+
+            dcc.Graph(id='month_year', figure={})],
+            xs=12, sm=12, md=12, lg=12, xl=12
+                )
+    ]),
+
+    dbc.Row([
+
+        dbc.Col([
             dcc.Dropdown(
                 id='year',
                 options=[{'label':x, 'value':x}
                                    for x in sorted(month_year_group['year'].unique())],
-
                 multi=False,
                 value=2020,
                 style={'width':'40%'}
@@ -49,27 +62,27 @@ app.layout = dbc.Container([
             xs=12, sm=12, md=12, lg=6, xl=6
                 ),
 
-       dbc.Col([
+        dbc.Col([
+        #     dcc.Dropdown(
+        #         id='year3',
+        #         options=[{'label':x, 'value':x}
+        #                            for x in sorted(month_year_group['year'].unique())],
+        #         multi=False,
+        #         value=2020,
+        #         style={'width':'40%'}
+        #         ),
 
-            dcc.Checklist(id='year2', value=[2018,2019,2020],
-                          options=[{'label':x, 'value':x}
-                                   for x in sorted(month_year_group['year'].unique())],
-                          labelClassName="mr-3"),
-
-            dcc.Graph(id='month_year', figure={})],
+            dcc.Graph(id='customer_type', figure={})],
             xs=12, sm=12, md=12, lg=6, xl=6
                 )
 
-    ], justify='center'),
+    ], align='center'),
 
     dbc.Row([
 
         ])
 
-
-    ])
-
-
+])
 
 
 # ----- figure 1 call back and plot
@@ -89,8 +102,9 @@ def update_graph(option_selected):
         dff,
         values='sales_amount',
         names='markets_name',
-        title=f'revenue by market for year {option_selected}'
+        title=f'revenue by market for year {option_selected}',
         )
+    fig.update_traces(textposition='inside', textinfo='percent+label')
 
     return fig
 
@@ -102,12 +116,38 @@ def update_graph(option_selected):
 )
 
 def update_graph(values):
-    print(values)
     temp = month_year_group[month_year_group['year'].isin(values)]
     fig2 = px.line(temp, x="month_name", y='sales_amount', color='year')
     fig2.update_xaxes(type='category', tick0='January')
-
+    fig2.update_layout(hovermode="x")
     return fig2
 
+
+# ----- figure 3 -----
+@app.callback(
+    Output('customer_type', 'figure'),
+    Input('year', 'value')
+)
+
+def update_graph(option_selected):
+    dff = df.copy()
+    dff = dff[dff['year']== option_selected]
+
+    customer_type_sum = dff.groupby(['customer_type'])['sales_amount'].sum().reset_index()
+    bm = customer_type_sum.iloc[0][1].round(2)
+    ec = customer_type_sum.iloc[1][1].round(2)
+
+    customer_sales = dff.groupby(['custmer_name','customer_type'])['sales_amount'].sum().reset_index()
+
+    fig = px.bar(customer_sales, x='custmer_name', y='sales_amount', color='customer_type',
+                title = f'Brick & Mortar sales: {bm:,} , E-Commerce sales: {ec:,}')
+    fig.update_layout(hovermode="x")
+
+    return fig
+
 if __name__ == '__main__':
-  app.run_server()
+
+    app.run_server(debug=True)
+
+    #TODO
+    # check spelling of custmer_names to customer_names. line142
