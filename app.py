f9f0cfec2954ec7591f4caac1de5da35a002b779
Terrence Bosco <terrencebosco@gmail.com>
2021-01-16 21:28:45 -0500

    started work on yearly page.. check multi page dash app git for code

--- app.py
+++ app.py
@@ -47,47 +47,14 @@ app.layout = dbc.Container([
             dcc.Graph(id='month_year', figure={})],
             xs=12, sm=12, md=12, lg=12, xl=12
                 )
-    ]),
-
+            ]),
+# ])
     # second row of graphs
     dbc.Row([
 
         # pie: year breakdown revenue by market
         dbc.Col([
-            dcc.Dropdown(
-                id='year',
-                options=[{'label':x, 'value':x}
-                                   for x in sorted(month_year_group['year'].unique())],
-                multi=False,
-                value=2020,
-                style={'width':'50%'}
-                ),
-
-            dcc.Graph(id='revenue', figure={})],
-            xs=12, sm=12, md=12, lg=6, xl=6
-                ),
-
-        # bar: customer contrbution by seperated by buisness type.
-        dbc.Col([
-            dcc.Graph(id='customer_type', figure={})],
-            xs=12, sm=12, md=12, lg=6, xl=6
-                )
-
-    ], align='center'),
-
-    dbc.Row([
-        dbc.Col([
-
-            dcc.Dropdown(
-                id='year3',
-                options=[{'label':x, 'value':x}
-                                   for x in sorted(month_year_group['year'].unique())],
-                multi=False,
-                value=2020,
-                style={'width':'62%'}
-                ),
-
-            dcc.Input(
+             dcc.Input(
                 id='look_at',
                 placeholder='Value between 0 and 259',
                 type='number',
@@ -96,49 +63,80 @@ app.layout = dbc.Container([
                 max=259
                 ),
 
-            dcc.Dropdown(
-                id='market',
-                options=[{'label':x, 'value':x}
-                                   for x in sorted(df['markets_name'].unique())],
-                multi=False,
-                value='Mumbai',
-                style={'width':'62%'}
-                ),
-
-            dcc.Graph(id='market_product_bar', figure={})],
+            dcc.Graph(id='product_sales', figure={})],
             xs=12, sm=12, md=12, lg=6, xl=6
                 ),
 
         dbc.Col([
-            dcc.Graph(id='market_product_pie', figure={})],
+            dcc.Graph(id='product_sales_pie', figure={})],
             xs=12, sm=12, md=12, lg=6, xl=6
-                )
+        )
 
-        ],align='center')
-
+    ], align='center'),
 ])
+#     dbc.Row([
+#         dbc.Col([
+
+#             dcc.Dropdown(
+#                 id='year3',
+#                 options=[{'label':x, 'value':x}
+#                                    for x in sorted(month_year_group['year'].unique())],
+#                 multi=False,
+#                 value=2020,
+#                 style={'width':'62%'}
+#                 ),
+
+#             dcc.Input(
+#                 id='look_at',
+#                 placeholder='Value between 0 and 259',
+#                 type='number',
+#                 value=5,
+#                 min=0,
+#                 max=259
+#                 ),
+
+#             dcc.Dropdown(
+#                 id='market',
+#                 options=[{'label':x, 'value':x}
+#                                    for x in sorted(df['markets_name'].unique())],
+#                 multi=False,
+#                 value='Mumbai',
+#                 style={'width':'62%'}
+#                 ),
+
+#             dcc.Graph(id='market_product_bar', figure={})],
+#             xs=12, sm=12, md=12, lg=6, xl=6
+#                 ),
+
+#         dbc.Col([
+#             dcc.Graph(id='market_product_pie', figure={})],
+#             xs=12, sm=12, md=12, lg=6, xl=6
+#                 )
+
+#         ],align='center')
+
+
 
 
 # ----- figure 1 call back and plot
 @app.callback(
     # outputs 1
-    Output('revenue','figure'),
+    Output('product_sales','figure'),
     # input from user
-    Input('year', 'value')
+    [Input('year2', 'value'),
+    Input('look_at', 'value')]
 )
 
-def update_graph(option_selected):
+def update_graph(year, look_at):
 
-    dff = df.copy()
-    dff = dff[dff['year']== option_selected]
+    if year == None:
+        year = 2018
 
-    fig = px.pie(
-        dff,
-        values='sales_amount',
-        names='markets_name',
-        title=f'revenue by market for year {option_selected}',
-        )
-    fig.update_traces(textposition='inside', textinfo='percent+label')
+    temp = df[df['year'].isin(year)]
+    product_group = temp.groupby('product_code')['sales_amount'] \
+    .sum().reset_index().sort_values('sales_amount',ascending=False)[:look_at]
+
+    fig = px.bar(product_group, y='sales_amount', x='product_code')
 
     return fig
 
@@ -149,8 +147,8 @@ def update_graph(option_selected):
     Input('year2', 'value')
 )
 
-def update_graph(values):
-    temp = month_year_group[month_year_group['year'].isin(values)]
+def update_graph(year):
+    temp = month_year_group[month_year_group['year'].isin(year)]
     fig2 = px.line(temp, x="month_name", y='sales_amount', color='year')
     fig2.update_xaxes(type='category', tick0='January')
     fig2.update_layout(hovermode="x")
@@ -159,74 +157,98 @@ def update_graph(values):
 
 # ----- figure 3 -----
 @app.callback(
-    Output('customer_type', 'figure'),
-    Input('year', 'value')
+    # outputs 1
+    Output('product_sales_pie','figure'),
+    # input from user
+    [Input('year2', 'value'),
+    Input('look_at', 'value')]
 )
 
-def update_graph(option_selected):
-    dff = df.copy()
-    dff = dff[dff['year']== option_selected]
+def update_graph(year, look_at):
 
-    customer_type_sum = dff.groupby(['customer_type'])['sales_amount'].sum().reset_index()
-    bm = customer_type_sum.iloc[0][1].round(2)
-    ec = customer_type_sum.iloc[1][1].round(2)
+    if look_at == None:
+        look_at = 0
 
-    customer_sales = dff.groupby(['custmer_name','customer_type'])['sales_amount'].sum().reset_index()
+    temp = df[df['year'].isin(year)]
+    product_group = temp.groupby('product_code')['sales_amount'].sum().reset_index() \
+    .sort_values('sales_amount',ascending=False)
 
-    fig = px.bar(customer_sales, x='custmer_name', y='sales_amount', color='customer_type',
-                title = f'Brick & Mortar sales: {bm:,} , E-Commerce sales: {ec:,}')
-    fig.update_layout(hovermode="x")
-
-    return fig
-
-# ----- figure 4 -----
-@app.callback(
-    Output('market_product_bar', 'figure'),
-    [Input('year3', 'value'),
-    Input('look_at', 'value'),
-    Input('market', 'value')]
-)
-
-def update_graph(year, look_at, market):
-
-    # data query
-    dff = df.query(f'year=={year}')
-    total_products = total_products = len(dff[dff['markets_name']==market]['product_code'].unique())
-
-    temp = dff[dff['markets_name']==market]['product_code'].value_counts().head(look_at)
-    fig = px.bar(temp, y=temp.values, x=temp.index,
-                 title=f'top {look_at} out of {total_products} products sold in {market}')
-
-    return fig
-
-# ----- figure 5 ----
-@app.callback(
-    Output('market_product_pie', 'figure'),
-    [Input('year3', 'value'),
-    Input('look_at', 'value'),
-    Input('market', 'value')]
-)
-
-def update_graph(year, look_at, market):
-
-    #data
-    dff = df.query(f'year=={year}')
-
-    # get n out of total to show percent of total
-    total = len(dff['product_code'].unique())
+    total = len(temp['product_code'].unique())
     zeros = np.zeros(total)
-    pull = [.1] * int(look_at)
-    test = list(np.concatenate([pull, zeros[10:]]))
+    pull = [.1] * look_at
+    test = list(np.concatenate([pull, zeros[look_at:]]))
 
-    # expand 'look_at' amount for pie chart
-    explode = len(dff['product_code'].unique())
-    product_out_of_100 = dff[dff['markets_name']==market]['product_code'].value_counts()
+    explode = len(product_group['product_code'].unique())
 
-    product_out_of_100.sum()
-    fig = go.Figure(data=[go.Pie(labels=product_out_of_100.index, values=product_out_of_100.values, pull=test)])
-    fig.update_traces(textposition='inside', textinfo='percent+label')
+    fig = go.Figure(data=[go.Pie(labels=product_group['product_code'], values=product_group['sales_amount'], pull=test)])
+    fig.update_traces(textposition='inside', textinfo='percent+label')\
 
     return fig
+
+# def update_graph(option_selected):
+#     dff = df.copy()
+#     dff = dff[dff['year']== option_selected]
+
+#     customer_type_sum = dff.groupby(['customer_type'])['sales_amount'].sum().reset_index()
+#     bm = customer_type_sum.iloc[0][1].round(2)
+#     ec = customer_type_sum.iloc[1][1].round(2)
+
+#     customer_sales = dff.groupby(['custmer_name','customer_type'])['sales_amount'].sum().reset_index()
+
+#     fig = px.bar(customer_sales, x='custmer_name', y='sales_amount', color='customer_type',
+#                 title = f'Brick & Mortar sales: {bm:,} , E-Commerce sales: {ec:,}')
+#     fig.update_layout(hovermode="x")
+
+#     return fig
+
+# # ----- figure 4 -----
+# @app.callback(
+#     Output('market_product_bar', 'figure'),
+#     [Input('year3', 'value'),
+#     Input('look_at', 'value'),
+#     Input('market', 'value')]
+# )
+
+# def update_graph(year, look_at, market):
+
+#     # data query
+#     dff = df.query(f'year=={year}')
+#     total_products = total_products = len(dff[dff['markets_name']==market]['product_code'].unique())
+
+#     temp = dff[dff['markets_name']==market]['product_code'].value_counts().head(look_at)
+#     fig = px.bar(temp, y=temp.values, x=temp.index,
+#                  title=f'top {look_at} out of {total_products} products sold in {market}')
+
+#     return fig
+
+# # ----- figure 5 ----
+# @app.callback(
+#     Output('market_product_pie', 'figure'),
+#     [Input('year3', 'value'),
+#     Input('look_at', 'value'),
+#     Input('market', 'value')]
+# )
+
+# def update_graph(year, look_at, market):
+
+#     #data
+#     dff = df.query(f'year=={year}')
+
+#     # get n out of total to show percent of total
+#     total = len(dff['product_code'].unique())
+#     zeros = np.zeros(total)
+#     pull = [.1] * int(look_at)
+#     test = list(np.concatenate([pull, zeros[10:]]))
+
+#     # expand 'look_at' amount for pie chart
+#     explode = len(dff['product_code'].unique())
+#     product_out_of_100 = dff[dff['markets_name']==market]['product_code'].value_counts()
+
+#     product_out_of_100.sum()
+#     fig = go.Figure(data=[go.Pie(labels=product_out_of_100.index, values=product_out_of_100.values, pull=test)])
+#     fig.update_traces(textposition='inside', textinfo='percent+label')
+
+#     return fig
 
 if __name__ == '__main__':
 
